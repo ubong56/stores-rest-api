7c2502efda251983915947f40cd4c66d2558dbdf
ubong57 <ubong.inyang@halliburton.com>
2021-08-03 14:59:36 -0500

    Fixed not importing db when running the app

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append(r"C:\Users\hb21315\PycharmProjects\test\rest-apis-flask-python-master\section4")
+
 sys.path
 
 from flask import Flask, request, jsonify
@@ -26,9 +26,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_AUTH_USERNAME_KEY'] = 'email'
 jwt = JWT(app, authenticate, identity_function)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 
 
@@ -46,68 +43,7 @@ def customized_error_handler(error):
  'code': error.status_code
  }), error.status_code
 
-# class Student(Resource):
-#     def get (self, name):
-#         return {'student': name}
-#
-#
-# api.add_resource(Student, '/student/<string:name>')
-#
-# app.run(port=5000)
 
-# items = []
-#
-# class Item(Resource):
-#     parser = reqparse.RequestParser()
-#     parser.add_argument('price',
-#                         type=float,
-#                         required=True,
-#                         help="This field cannot be left blank!"
-#                         )
-#
-#     @jwt_required()
-#     def get(self, name):
-#         # for item in items:
-#         #     if item['name'] == name:
-#         #         return item
-#         # return {'item': None}, 404
-#         item = next(filter(lambda x: x['name'] == name, items), None)
-#
-#         return {'item':  item}, 200 if item else 404
-#
-#     def post(self, name):
-#         # data = request.get_json()
-#         if next(filter(lambda x: x['name'] == name, items), None) is not None:
-#             return {'message': "An item with name '{}' already exists.".format(name)}, 400
-#         # data = request.get_json()
-#         data = Item.parser.parse_args()
-#
-#         item = {'name': name, 'price': data['price']}
-#         items.append(item)
-#         return item, 201
-#
-#     @jwt_required()
-#     def delete(self, name):
-#         global items
-#         items = list(filter(lambda x: x['name'] != name, items))
-#         return {'message': 'Item deleted'}
-#
-#     @jwt_required()
-#     def put(self, name):
-#         data = Item.parser.parse_args()
-#         # data = request.get_json()
-#         # Once again, print something not in the args to verify everything works
-#         item = next(filter(lambda x: x['name'] == name, items), None)
-#         if item is None:
-#             item = {'name': name, 'price': data['price']}
-#             items.append(item)
-#         else:
-#             item.update(data)
-#         return item
-#
-# class ItemList(Resource):
-#     def get(self):
-#         return {'items': items}
 
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
